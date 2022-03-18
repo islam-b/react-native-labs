d304fbcb409e433a4b6bab0eb7edfcc13d271a57
islam.bouayache <islam.bouayache@itcomp-dz.com>
2022-03-18 17:06:55 +0100

    Add errors interceptor

--- src/data/datasources/index.js
+++ src/data/datasources/index.js
@@ -1,6 +1,14 @@
 import ENV from "../../../envrionment.";
 import axios from "axios";
+import {Alert} from "react-native"
 
 export const HttpClient = axios.create({ baseURL: ENV.baseUrl });
 
- 
+HttpClient.interceptors.response.use(
+    response=>response,
+    error=>{
+        console.log(error)
+        Alert.alert("An error has occured !")
+        return Promise.reject(error)
+    }
+)
