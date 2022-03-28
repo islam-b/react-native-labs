43a214afa265a47926452b9b9baee7c8fa148c8c
islam.bouayache <islam.bouayache@itcomp-dz.com>
2022-03-28 08:32:09 +0100

    Fix

--- App.js
+++ App.js
@@ -1,21 +1,12 @@
 import React from 'react'
 import { SafeAreaView, StatusBar, View, Dimensions } from "react-native"
-import { Appbar, Provider as PaperProvider } from 'react-native-paper';
 import { BooksList } from "./src/screens/BooksList"
 
 const App = () => {
   return (
-    <PaperProvider >
-      <SafeAreaView style={{ display: 'flex' }}>
-        <StatusBar />
-        <Appbar.Header>
-          <Appbar.Content title="BooksApp" />
-        </Appbar.Header>
-        <View style={{ height: Dimensions.get('window').height }}>
+        <View >
           <BooksList />  
         </View>
-      </SafeAreaView>
-    </PaperProvider >
   );
 };
 
